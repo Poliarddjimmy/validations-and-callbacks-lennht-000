56e183985919ca9c10a3002d48c7ffe3f1f6a81b
Djimmy Poliard <djimmypoliard@gmail.com>
2023-08-21 00:12:31 -0400

    push

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
 
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :age, numericality: { greater_than: 0 }
+
+  before_save :capitalize_name
+
   private
 
   def capitalize_name
